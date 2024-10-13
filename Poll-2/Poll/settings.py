1981b298882a0b2f39aecc8b7be71a91225921ce
Anna Kryshnia <151573670+AKryshnia@users.noreply.github.com>
2024-10-13 19:53:38 +0300

    Update settings.py

--- Poll-2/Poll/settings.py
+++ Poll-2/Poll/settings.py
@@ -28,11 +28,11 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost',
+    '92.53.120.77',
+    'akryshnia-django-survey-app-2b4e.twc1.net',
 ]
 
 
@@ -90,12 +90,6 @@ WSGI_APPLICATION = 'Poll.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     "default": {
